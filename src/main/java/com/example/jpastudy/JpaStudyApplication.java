5d655671aa47842aa83c7f57881bc47ee0845504
redjen8 <seokwoo2525@gmail.com>
2022-07-18 13:29:07 +0900

    20220718 jsw Feat : jpa 쿼리 테스트 수행

--- src/main/java/com/example/jpastudy/JpaStudyApplication.java
+++ src/main/java/com/example/jpastudy/JpaStudyApplication.java
@@ -1,5 +1,10 @@
 package com.example.jpastudy;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +12,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpaStudyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JpaStudyApplication.class, args);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Member member = new Member();
+
+        member.setId(1L);
+        member.setName("Hello");
+
+        em.persist(member);
+        tx.commit();
+        em.close();
+        emf.close();
     }
 
 }
